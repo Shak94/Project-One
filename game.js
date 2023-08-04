7f6c1f3107e7e30823435f09658a76423904c15d
Shak94 <shakoi.1994@gmail.com>
2023-08-04 11:09:52 -0400

    finally debugged game

--- game.js
+++ game.js
@@ -1,129 +1,133 @@
-//I will beging by grabbing my classes from my HTML file, so i can manipulate my DOM.
 const game = document.querySelector(".play-area");
 const listOfLines = document.querySelectorAll(".line");
 const cards = document.querySelectorAll(".card");
-// console.log(cards)
 const frontOfCard = document.querySelectorAll(".front");
 const backOfcard = document.querySelectorAll(".back");
 const suspects = document.querySelectorAll(".character");
-console.log(suspects);
 const flipSomeCards = document.querySelector(".hint");
-// console.log(flipSomeCards);
 const startButton = document.querySelector(".start");
-// console.log(startButton);
 const resetButton = document.querySelector(".reset");
 let clock = document.querySelector(".timer")
-// console.log(clock);
 const matchAttempts = document.querySelector('.attempts')
 const matchesCount = document.querySelector(".correctMatches");
-const showCard=document.querySelector("showCard");
-let cardOne, cardTwo;
+const showCard = document.querySelector(".showCard");
+let lockBoard = false;
+let cardOne = false;
+let cardTwo = false;
 let cardsGuess = [];
 let correctMatches = [];
 let playerMatchAttempts = 0;
 let playerMatchCount = 0;
-//Flip Card Function here.
-function flipCard(card) {
-    card.classList.add("showCard")
+let faceUpCards = [];
+
+function resetGame() {
+  // Clear the selected cards
+  cardOne = null;
+  cardTwo = null;
+  cardsGuess = [];
+  correctMatches = [];
+  playerMatchAttempts = 0;
+  playerMatchCount = 0;
+  faceUpCards = [];
+
+  // Hide all the cards
+  cards.forEach(card => card.classList.remove('showCard'));
+
+  // Reset attempts and matches display
+  matchAttempts.innerHTML = 'Match attempts: 0';
+  matchesCount.innerHTML = 'Matches: 0';
+}
+function flipCard() {
+    if (this.classList.contains('showCard') || cardsGuess.length  >= 2 || lockBoard ) {
+        return;
+    }
+
+    this.classList.add('showCard');
+
     if (!cardOne) {
-        cardOne = card.dataset.framework
-        console.log("Card one flipped:", cardOne);
-        cardsGuess.push(cardOne);
+        cardOne = this;
+        console.log('Check what card one is here', cardOne);
     } else {
-        cardTwo = card.dataset.framework
-        console.log("Card two flipped:", cardTwo);
-        cardsGuess.push(cardTwo);
-        matchAttempts.innerHTML = (`Match attempts:${playerMatchAttempts}`)
+        cardTwo = this;
+        console.log('Check what card two is here', cardTwo);
         playerMatchAttempts++;
-        checkMatch();
+        matchAttempts.innerHTML = `Match attempts: ${playerMatchAttempts}`;
+        checkMatch(this);
+        faceUpCards.push(this);
     }
 }
-        // console.log(playerMatchAttempts)
-        // console.log("checking ", cardOne)
-        //Compare CardOne and CardTwo to check for a successfulmatch.
-        function checkMatch(card){
-        if (cardOne === cardTwo ) {
-            playerMatchCount++;
-            setTimeout(() => {
-                cards.forEach(card => card.removeEventListener('click', flipCard));
-            },3000);
-            matchesCount.innerHTML = (`Matches:${playerMatchCount}`);
-            console.log('YOU HAVE A MATCH');
-            correctMatches.push(cardOne, cardTwo);
-            console.log("card Guess", cardsGuess);
-            cardsGuess = [];
-          
-            removeListener();
-            
 
-        }
-        else {
-            cardOne=null;
-            cardTwo=null;
-
-            console.log('NOT A MATCH');
-            setTimeout(() => {
-                cards.forEach((card) =>{
-                    card.classList.remove("showCard")
-                });
-               
-            }, 1500);
-            console.log(cardOne ," Check what card one is here")
-        }
-
-    
+function checkMatch(card) {
+    if (!cardOne) {
+        cardOne = card;
+        return;
     }
 
-function removeListener(){
+    cardTwo = card;
+
+
+    if (cardOne.dataset.framework === cardTwo.dataset.framework) {
+        playerMatchCount++;
+        matchesCount.innerHTML = `Matches: ${playerMatchCount}`;
+        console.log('YOU HAVE A MATCH');
+        correctMatches.push(cardOne, cardTwo);
+        cardsGuess = [];
+        faceUpCards = faceUpCards.filter((card) => card !== cardOne && card !== cardTwo);
+
+        cardOne = null;
+        cardTwo = null;
+    } else {
+        console.log('NOT A MATCH');
+
+        setTimeout(() => {
+          cardOne.classList.remove("showCard"); 
+          cardTwo.classList.remove("showCard"); 
+            cardOne = null;
+            cardTwo = null;
+            lockBoard = false;
+        }, 1500);
+    }
+}
+
+function removeListener() {
     for (let i = 0; i < cards.length; i++) {
         cards[i].removeEventListener("click", () => {
             flipCard(cards[i]);
         });
-}
-};
-console.log(removeListener, "checking function")
-//Win Conditions!.
-function declareWinner() {
-    for (let i = 0; i < correctMatches.length; i++) {
-        if (correctMatches.length[i]/ 2 === playerMatchCount.innerHTML) {
-            // console.log('You have found the killer Congratz')
-
-        }
-
     }
 }
-console.log(declareWinner, "checking win condition function")
 
-// console.log(cardsGuess);
-// console.log(flipCard.cardsGuess);
-
+function declareWinner() {
+    for (let i = 0; i < correctMatches.length; i++) {
+        if (correctMatches.length/ 2 === playerMatchCount.innerHTML) {
+            // Add your code for declaring the winner here.
+        }
+    }
+}
 
 console.log('flipBack')
 console.log("flipCard");
 
-// Flip All Cards with Hint button.
 function flipAllCards() {
     for (let i = 0; i < cards.length; i++) {
-        flipCard(cards[i]);
+        cards[i].click();
     }
 }
+
 function handleHintClick() {
-    flipAllCards();
-    setTimeout(flipAllCards, 500);
-    cards.forEach(card => {
-        
+  flipAllCards();
+  setTimeout(() => {
+    faceUpCards.forEach((card) => {
+      card.classList.remove('showCard');
     });
+    faceUpCards = []; 
+  }, 500);
 }
-// console.log(handleHintClick)
-flipSomeCards.addEventListener("click", () => {
-    handleHintClick();
-});
-//Start Game will start the timer and inistialize game play.
+
 function startTimer(seconds) {
     let myTimer = seconds;
     const interval = setInterval(() => {
         console.log(myTimer);
-        // console.log(interval);
         clock.innerHTML = (`${myTimer}`)
         myTimer--;
         if (myTimer < 0) {
@@ -131,38 +135,34 @@ function startTimer(seconds) {
             clock.innerHTML = ('THE KILLER GOT AWAY!');
         }
     }, 1000);
- 
-
 }
 
-// console.log(startTimer)
-
-// Clear game/Clear class attributes
-
-//Reset Cards/Shuffle
 function randomCards(card) {
-    cards.forEach(card => {
-        let randomizedCards = Math.floor(Math.random() * 16);
-        card.style.order = randomizedCards;
-
-    })
-}
-// console.log(randomCards)
-//
-//Event listeners
-
-for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", () => {
-        flipCard(cards[i]);
-    });
-    // console.log(cards[i]);
+  cards.forEach(card => {
+      let randomizedCards = Math.floor(Math.random() * 16);
+      card.style.order = randomizedCards;
+  });
 }
 
-startButton.addEventListener('click', () => (startTimer(60)));
-// console.log(cardOne)
-// console.log(cardTwo)
+function removeClickListeners() {
+  cards.forEach(card => {
+      card.removeEventListener('click', flipCard);
+  });
+}
+
+function addClickListeners() {
+  cards.forEach(card => {
+      card.addEventListener('click', flipCard);
+  });
+}
+
+startButton.addEventListener('click', () => {
 randomCards();
-resetButton.addEventListener('click', () => randomCards(cards));
-// console.log(resetButton);
-checkMatch();
-cards.forEach(card => card.addEventListener('click', flipCard));
+startTimer(60);
+removeClickListeners();
+addClickListeners();
+});
+
+resetButton.addEventListener('click', () => {
+  resetGame();
+});
